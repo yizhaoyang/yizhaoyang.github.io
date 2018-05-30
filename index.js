c61950338bd7188eca3bbbff9dc75ef9858de351
yizhaoyang <yizhaoyang1224@gmail.com>
2018-05-30 12:01:52 +0200

    progess bar

--- index.js
+++ index.js
@@ -10,3 +10,31 @@ setInterval(function(){
     document.querySelector("h3").style.color="#005eff";
     document.querySelector("h3").style.top="200px";
 },3000);
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+
+
+
+  $(".see-more").nextUntil(".see-less").wrapAll("<div class='see-more-content'></div>");
+
+
+  $(".see-less").hide();
+  var count= 1
+  $(".see-more-content").slideUp(0);
+
+
+
+$(".see-more").click(function(){
+
+    $(".see-more-content").slideToggle();
+    $(".see-more").hide();
+    $(".see-less").show();
+
+});
+$(".see-less").click(function(){
+    $(".see-more-content").slideToggle();
+    $(".see-less").hide();
+    $(".see-more").show();
+});

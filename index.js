eb9dd1e7da2ec1821d4431808440e3f3b05efdc8
yizhaoyang <yizhaoyang1224@gmail.com>
2018-05-30 16:50:11 +0200

    ojdas

--- index.js
+++ index.js
@@ -16,22 +16,27 @@ $(function () {
   })
 
 
+  function myFunction() {
+    var x = document.getElementById("showmore");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
 
-  $(".see-more").nextUntil(".see-less").wrapAll("<div class='see-more-content'></div>");
-
-
-  $(".see-less").hide();
-  var count= 1
-  $(".see-more-content").slideUp(0);
-
-
-
+$(".see-more").nextUntil(".see-less").wrapAll("<div class='see-more-content'></div>");
+    
+$(".see-less").hide();
+var count= 1
+$(".see-more-content").slideUp(0);
+    
 $(".see-more").click(function(){
-
+    
     $(".see-more-content").slideToggle();
     $(".see-more").hide();
     $(".see-less").show();
-
+    
 });
 $(".see-less").click(function(){
     $(".see-more-content").slideToggle();
